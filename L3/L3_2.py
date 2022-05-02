f593128defad9674f38db7d333965087eef413a5
rokas_simon <rokas.rokanas@gmail.com>
2022-05-02 09:47:17 +0300

    Tensorflow

--- L3/L3_2.py
+++ L3/L3_2.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from keras import Sequential
 
 def main():
 
     df = pd.read_csv('normalized.csv')
 
-    
+    inputCols = df.iloc[1, 5:]
+
+    print(inputCols)
 
     return
