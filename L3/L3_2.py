3005cade565dd0d83eb95bb258267279e3595198
rokas_simon <rokas.rokanas@gmail.com>
2022-05-08 21:11:49 +0300

    Finished L3

--- L3/L3_2.py
+++ L3/L3_2.py
@@ -1,32 +1,56 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras.optimizer_v2.adam import Adam
 from keras import Sequential
 from keras.layers import Dense
-import keras.optimizers
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import KFold
+
+tf.random.set_seed(1)
+np.random.seed(1)
+
+np.set_printoptions(suppress=True)
 
 def main():
 
-    df = pd.read_csv('normalized.csv')
-    inputColumns = ['danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'liveness', 'valence', 'tempo', 'key', 'mode']
-    outputColumn = ['Popularity']
+    df = pd.read_csv('corrected.csv')
+    inputColumns = ['loudness', 'acousticness']
+    outputColumn = ['energy']
 
     X = df[inputColumns].to_numpy()
     Y = df[outputColumn].to_numpy()
 
-    xtrain, xtest, ytrain, ytest = train_test_split(X, Y, test_size=0.3, random_state=0)
+    percentToUse = 1
+    n = int(len(X) * percentToUse)
 
-    model = Sequential()
-    model.add(Dense(2, activation='sigmoid', input_dim=X.shape[1]))
-    model.add(Dense(1, activation='sigmoid'))
-    opt = keras.optimizers.sgd_experimental.SGD(0.01)
-    #model.compile(optimizer='SGD', loss='mean_squared_error', metrics=['accuracy'])
-    model.compile(optimizer=opt, loss='mean_squared_error', metrics=['accuracy'])
-    #print(model.get_weights())
+    X = X[:n]
+    Y = Y[:n]
 
-    res = model.fit(xtrain, ytrain, batch_size=10, epochs=50, verbose=1, validation_split=0.2)
+    K = 10
+    kFold = KFold(K)
 
-    #print(model.get_weights())
+    resultMSEs = []
+
+    for trainIdx, testIdx in kFold.split(X, Y):
+        tf.random.set_seed(1)
+        np.random.seed(1)
+
+        model = Sequential()
+        model.add(Dense(46, activation='relu'))
+        model.add(Dense(1))
+        opt = Adam(0.001)
+        model.compile(optimizer=opt, loss='mean_squared_error', metrics=['mse'])
+
+        model.fit(X[trainIdx], Y[trainIdx], batch_size=1, epochs=30, verbose=1, validation_split=0.2)
+
+        mse = model.evaluate(X[testIdx], Y[testIdx])[0]
+
+        print(mse)
+        resultMSEs.append(mse)
+
+    print(resultMSEs)
+    print(np.mean(resultMSEs))
 
     return
 
